b3b22c88cbff01e512eebfa0fe902dd287d93159
StarProxima <maks.prozorov1@yandex.ru>
2023-02-06 03:42:08 +0300

    Lab 1 - Task 3 - Subtask 2

---
+++ lab1/task3/st2.rb
@@ -0,0 +1,16 @@
+require "./st1.rb"
+
+if ARGV.length != 2
+    puts "Usage: ruby st2.rb <number of method> <file>"
+    exit
+end
+
+array = File.read(ARGV[1]).split.map(&:to_i)
+
+puts( case ARGV[0].to_i
+        when 0
+            find_minimum(array)
+        when 1
+            find_number_of_first_positive_element(array)
+        end
+    )
