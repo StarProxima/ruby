3bbf345950b598e5f3513332ad219e46c9c2ed6b
StarProxima <maks.prozorov1@yandex.ru>
2023-02-06 04:42:02 +0300

    Fix Lab 1 - Task 4 - Subtask 4

--- lab1/task4/st4.rb
+++ lab1/task4/st4.rb
@@ -1,13 +1,16 @@
 # 1.45 Дан целочисленный массив и интервал a..b. Необходимо найти сумму элементов, значение которых попадает в этот интервал.
 
-def sum_in_range(arr, range)
-    arr[range].reduce(0) { |s, x| s + x }
-end
 
 def sum_in_interval(arr, a, b)
-    arr[a..b].reduce(0) { |s, x| s + x }
+    arr.reduce(0) { 
+        |s, x| 
+        if x >= a && x <= b then 
+            s + x 
+        else 
+            s 
+        end
+    }
 end
 
 
-# puts sum_in_range([1, 2, 3, 4], 1..2)
-# puts sum_in_interval([1, 2, 3, 4], 1, 2)
+puts sum_in_interval([1, 2, 3, 4], 1, 2)
